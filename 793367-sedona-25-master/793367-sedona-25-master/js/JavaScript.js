8cbe61033f1da386ebd323ffbf57952a0ddcdd65
Lores <83615126+Lores7@users.noreply.github.com>
2021-06-09 23:07:33 +0300

    ...

--- 793367-sedona-25-master/793367-sedona-25-master/js/JavaScript.js
+++
@@ -1,9 +0,0 @@
-var reservButton = document.querySelector(".search-button");
-var popup = document.querySelector(".hotel-reservation");
-
-popup.classList.add("hotel-reservation-none");
-
-reservButton.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.toggle("reserv-form");
-});
